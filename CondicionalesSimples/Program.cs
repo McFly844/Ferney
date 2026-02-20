2d4d248ed3e6b52a94cada825b7501f3cf1dea41
McFly844 <gallina.calva84@gmail.com>
2026-02-20 12:40:31 -0500

    Primeros archivos de la clase

---
+++ CondicionalesSimples/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondicionalesSimples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "";
+            float sueldo = 0f;
+            float edad = 0f;
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Ingrese el nombre del usuario");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            nombre = Console.ReadLine(); // Lo que llega desde la consola, es de tipo string
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ingrese edad del usario");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            edad = Convert.ToSingle (Console.ReadLine());
+
+            if (edad>=18)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{nombre} , bienvenido a mi pagina");
+            }
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Ingrese el sueldo del usuario");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            sueldo = Convert.ToSingle(Console.ReadLine());
+      
+
+            if (sueldo>=3000)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"La persona {nombre} , debe pagar impuestos");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            
+        
+            
+        }
+    }
+}
---
+++ Variablesconstantesytiposdedatos/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablesconstantesytiposdedatos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Enteros
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            int num1 = 5;
+            int num2 = -num1;
+            int num3 = -num2;
+            Console.WriteLine(num1 + "//" + num2 + "//" + num3);
+            //Aritmeticos
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int num4 = 3 + 5;
+            int num5 = 12 - 63;
+            int num6 = 26 * 14;
+            float num7 = 5 / 3f;
+            Console.WriteLine("\nSuma: {0}, Resta: {1}, Multiplicacion: {2}, Division: {3}",num4,num5,num6,num7);
+            //Incremento y decremento
+            num4++; // num4 + 1
+            num5--; // num5 - 1
+            num6 += 4; // num6 + 4
+            num4 -= 10; // num4 + 10
+            num5 *= 3; //num5 * 3
+            num6 /= 2; //num6 / 2
+            num6 *= num4; //num6 * num4
+            //Orden de evaluacion de operadores
+            int num8 = 4 * 3 / 2;
+            int num9 = 4 * (3 / 2);
+            int num10 = 4 + 6 * (2 / 1);
+            //Operadores logicos
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine"
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+    }
+}
