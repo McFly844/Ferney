458e7a31e72bd3cf0b3ebb383d468af9e0d0f6c0
McFly844 <gallina.calva84@gmail.com>
2026-03-11 13:30:59 -0500

    Add files via upload

---
+++ 9.AcumuladoryContador/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.AcumuladoryContador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int acumulador = 0;
+
+            while (contador<=5)
+            {
+                acumulador += contador;
+                Console.WriteLine($" Contador: {contador} - Acumulador: {acumulador}");
+
+                contador++;
+            }
+
+            Console.WriteLine($"La suma de los 5 primeros numeros enteros es: {acumulador}");
+        }
+    }
+}
