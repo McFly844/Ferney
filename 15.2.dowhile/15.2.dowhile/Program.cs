ab7de5686180da7a0e3411a18758e606cdde5033
Martin <gallina.calva84@gmail.com>
2026-03-20 13:29:53 -0500

    Add files via upload

---
+++ 15.2.dowhile/15.2.dowhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._2.dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string contraseñaCorrecta = "1234";
+            string contraseñaIngresada;
+            int intentos = 0;
+
+            Console.WriteLine("=== CONTRASEÑA ===");
+
+            do
+            {
+                Console.Write("Ingrese la contraseña: ");
+                contraseñaIngresada = Console.ReadLine();
+                intentos++;
+
+                if (contraseñaIngresada != contraseñaCorrecta)
+                {
+                    Console.WriteLine("¡Contraseña incorrecta! Intente nuevamente.\n");
+                }
+            }
+            while (contraseñaIngresada != contraseñaCorrecta);
+
+            Console.WriteLine($"\n¡Contraseña correcta!");
+            Console.WriteLine($"Número de intentos realizados: {intentos}");
+            Console.WriteLine("Presione cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
