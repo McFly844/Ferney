023e90c5cdf4147d79b92b27f42d5981c6de0b8f
McFly844 <gallina.calva84@gmail.com>
2026-03-20 12:41:04 -0500

    Add files via upload

---
+++ 14.dowhile/14.dowhile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.dowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contadordenumeros = 1;
+            int contador = 1;
+            int contadordivisor = 0;
+
+            do
+            {
+                do
+                {
+                    if (contadordenumeros%contador==0)
+                    {
+                        contadordivisor++;
+                    }
+                    contador++;
+
+                } while (contador<=contadordenumeros);
+
+                contador = 1;
+
+                if (true)
+                {
+                    Console.WriteLine(contadordenumeros);
+                    contadordenumeros++;
+
+                }
+                contadordivisor = 0;
+                contadordenumeros++;
+
+            } while (contadordenumeros<=100);
+        }
+    }
+}
