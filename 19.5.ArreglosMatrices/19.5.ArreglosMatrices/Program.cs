fa9820bc29551568d4f2928159d0d51eecf207bc
Martin <gallina.calva84@gmail.com>
2026-04-17 13:33:56 -0500

    Add files via upload

---
+++ 19.5.ArreglosMatrices/19.5.ArreglosMatrices/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._5.ArreglosMatrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz1 = new int[2, 3];
+            int[,] matriz2 = new int[2, 3];
+            int[,] matriz3 = new int[2, 3];
+
+        }
+    }
+}
