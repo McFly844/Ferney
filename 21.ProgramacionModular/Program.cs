bf63f7b08af23c0438606c80ca1c7c54065f3976
Martin <gallina.calva84@gmail.com>
2026-04-24 13:40:06 -0500

    Add files via upload

---
+++ 21.ProgramacionModular/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.ProgramacionModular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MostrarMenu();
+            int opcion = CapturarOpcion();
+            RealizarOpcion(opcion);
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("----------------------------MENU----------------------------");
+            Console.WriteLine("1.Suma                                            2.Resta");
+            Console.WriteLine("3.Multiplicacion                                  4.Division");
+            Console.WriteLine("------------------------------------------------------------");
+        }
+
+        static int CapturarOpcion()
+        {
+            Console.WriteLine("Ingrese una opcion del menu");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void RealizarOpcion(int opcion)
+        {
+            switch (opcion)
+            {
+                case 1:
+                    suma();
+                    break;
+                case 2:
+                    resta();
+                    break;
+                case 3:
+                    multiplicacion();
+                    break;
+                case 4:
+                    division();
+                    break;
+                default:
+                    Console.WriteLine("Ingrese una operacion valida");
+                    break;
+            }
+        }
+
+        static void suma()
+        {
+            Console.WriteLine("----------------------------SUMA----------------------------");
+        }
+
+        static void resta()
+        {
+            Console.WriteLine("---------------------------RESTA----------------------------");
+        }
+
+        static void multiplicacion()
+        {
+            Console.WriteLine("------------------------MULTIPLICACION-----------------------");
+        }
+
+        static void division()
+        {
+            Console.WriteLine("-------------------------DIVISION----------------------------");
+        }
+    }
+}
